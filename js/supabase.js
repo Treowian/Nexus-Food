ac0ccca30c87fc0e0540d1f70434bb80724adc9c
Treowian <odilon93@gmail.com>
2026-05-22 17:23:56 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // ⚠️ À REMPLACER par tes vraies clés Supabase
-const supabaseUrl = 'https://mherlbhfdeipvrztqexp.supabase.co/rest/v1/';
+const supabaseUrl = 'https://mherlbhfdeipvrztqexp.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oZXJsYmhmZGVpcHZyenRxZXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NjI3MzAsImV4cCI6MjA5NTAzODczMH0.gJCPDiBxk-Q3r_lnRRjTE7RVmIQ_KeFCz66jRyCAAQo';
 
 // Création et exportation du client de connexion
